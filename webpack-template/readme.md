702b036bf0380ad2eaa88a39a08cb9abd6ce64a0
nitesh <nitesh@voras.co.uk>
2019-05-24 15:27:21 +0100

    Update webpack and add plugins.

--- webpack-template/readme.md
+++ webpack-template/readme.md
@@ -11,3 +11,9 @@
 ## Usage
 
 'npm run start'
+
+### Notes
+
+- Dist folder is cleaned on build
+- Update webpack.config.js PWA info, app name, description, etc.
+- Update src/img/icons/site-icon.png to build app icons
--- webpack-template/webpack.config.js
+++ webpack-template/webpack.config.js
@@ -4,6 +4,8 @@ const StyleLintPlugin = require('stylelint-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = (env) => {
     const isProduction = env === 'production';
@@ -12,9 +14,9 @@ module.exports = (env) => {
         mode: isProduction ? 'production' : 'development',
         entry: './src/index.js',
         output: {
-            path: path.join(__dirname, 'dist/assets/'),
-            filename: 'bundle.js',
-            publicPath: '/assets/'
+            path: path.join(__dirname, 'dist'),
+            filename: 'assets/bundle.js',
+            publicPath: '/dist/'
         },
         module: {
             rules: [
@@ -79,14 +81,22 @@ module.exports = (env) => {
             ]
         },
         plugins: [
-            new MiniCssExtractPlugin(),
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: "assets/[name].css"
+            }),
             new StyleLintPlugin(),
-            new CopyWebpackPlugin([{
-                from: path.resolve(__dirname, 'src/img'),
-                to: path.resolve(__dirname, 'dist/assets/img')
-            }]),
+            new CopyWebpackPlugin([
+                {
+                    from: path.resolve(__dirname, 'src/img'),
+                    to: path.resolve(__dirname, 'dist/assets/img')
+                },
+                {
+                    from: path.resolve(__dirname, 'src/rootfiles'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+            ]),
             new ImageminPlugin({
-                //disable: process.env.NODE_ENV !== 'production', // Disable during development
                 pngquant: {
                     quality: '95-100'
                 }
@@ -94,6 +104,21 @@ module.exports = (env) => {
             new HtmlWebPackPlugin({
                 template: path.resolve(__dirname, 'src/index.html'),
                 filename: path.resolve(__dirname, 'dist/index.html')
+            }),
+            new WebpackPwaManifest({
+                name: 'My Progressive Web App',
+                short_name: 'MyPWA',
+                description: 'My awesome Progressive Web App!',
+                background_color: '#ffffff',
+                crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+                includeDirectory: false,
+                publicPath: '/',
+                icons: [
+                    {
+                        src: path.resolve('src/img/icons/site-icon.png'),
+                        sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+                    }
+                ]
             })
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',

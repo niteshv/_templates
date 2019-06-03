416bc6e2a326870cd54ac322ef284f59cb9c598f
nitesh <nitesh@voras.co.uk>
2019-06-03 14:20:00 +0100

    Carousel selector to data attribute

--- build/src/components/carousel/carousel.js
+++ build/src/components/carousel/carousel.js
@@ -2,8 +2,9 @@ import Flickity from 'flickity';
 import 'flickity/dist/flickity.min.css';
 
 const Carousel = () => {
+    const carouselItem = document.querySelector('[data-carousel]');
     window.addEventListener('load', () => {
-        const flkty = new Flickity('.carousel', {
+        const flkty = new Flickity( carouselItem, {
             // options
             autoPlay: true
         });

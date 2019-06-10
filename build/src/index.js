ca0bbbfbc9515a65fb3699c411858e9218b99999
nitesh <nitesh@voras.co.uk>
2019-06-10 17:16:23 +0100

    Move style imports into entry js.

--- build/src/index.js
+++ build/src/index.js
@@ -1,26 +1,27 @@
 //// INDEX
-// - CSS - Variables are imported via webpack.config, edit variables.scss
+// - CSS - Variables
 // - CSS - Normalize.css
 // - Component - Navigation menu button
 // - Component - Sticky Header
 // - Component - Carousels
 // - CSS - All other custom css
 
-
+// CSS - Variables
+import '../src/components/variables.scss';
 
 // CSS - normalize.css - import vendor css
 import 'normalize.css/normalize.css';
 
-// Navigation menu button
+// Component - Navigation menu button
 import Navigation from './components/nav/nav';
 const nav = new Navigation();
 nav.init();
 
-// Sticky Header
+// Component - Sticky Header
 import stickybits from 'stickybits';
 const stickybit = stickybits('#header', { useStickyClasses: true });
 
-// Carousels
+// Component - Carousels
 import CarouselStart from './components/carousel/carousel';
 const carousels = document.querySelectorAll('[data-carousel]');
 for (let carouselEl of carousels) {
@@ -29,4 +30,6 @@ for (let carouselEl of carousels) {
 }
 
 // CSS - Import all custom CSS styles - should be after all vendor css
-import '../src/components/styles.scss';
+import '../src/components/header/header.scss';
+import '../src/components/nav/nav.scss';
+import '../src/components/footer/footer.scss';

45dee7666207f927cccd08b2e9a19b13c55952ff
nitesh <nitesh@voras.co.uk>
2019-06-18 11:54:51 +0100

    Updates and tweaks

--- build/src/components/header/sticky.js
+++ build/src/components/header/sticky.js
@@ -3,18 +3,21 @@ import _ from 'lodash';
 class StickyHeader {
     constructor() {
         this.scrollpos = window.scrollY;
-        this.header = document.querySelector("header.header");
+        this.header = document.querySelector("header#header");
+        this.footer = document.querySelector("footer#footer");
     }
     init() {
-        window.onscroll = _.throttle( () => {
+        window.onscroll = _.throttle(() => {
             this.scrollpos = window.scrollY;
             if (this.scrollpos >= this.header.offsetHeight) {
                 // add class
                 this.header.classList.add("js-is-sticky");
+                this.footer.classList.add("js-is-sticky");
             }
             else {
                 // remove class
                 this.header.classList.remove("js-is-sticky");
+                this.footer.classList.remove("js-is-sticky");
             }
         }, 100);
     }
--- build/src/index.js
+++ build/src/index.js
@@ -1,16 +1,26 @@
 //// INDEX
+//
 // - CSS - Variables
 // - CSS - Normalize.css
+// - JS - Add js class to html
 // - Component - Navigation menu button
 // - Component - Sticky Header
 // - Component - Carousels
+// - JS - Smooth scroll
+// - JS - Load images
 // - CSS - All other custom css
+// - CSS - IE Fixes
+// - CSS - Print styles
+
 
 // CSS - Variables
 import './components/variables.scss';
 
 // CSS - normalize.css - import vendor css
 import 'normalize.css/normalize.css';
+
+// JS - Add js class to html
+document.querySelector('html.no-js').className = 'js';
 
 // Component - Navigation menu button
 import Navigation from './components/nav/nav';
@@ -30,11 +40,31 @@ for (let carouselEl of carousels) {
   carousel.init();
 }
 
-// CSS - Import all custom CSS styles - should be after all vendor css
+// JS - Smooth scroll
+import SmoothScroll from 'smooth-scroll';
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 400,
+    easing: 'easeOutQuad',
+    offset: function (anchor, toggle) {
+        const headerHeight = document.getElementById('header').getBoundingClientRect().height;
+        return headerHeight;
+    }
+});
+
+// JS - Load images
+import LazyLoad from "vanilla-lazyload";
+const lazyLoadOptions = {use_native: true};
+const pageLazyLoad = new LazyLoad();
+
+// CSS - Import all custom CSS style
 import './components/base/typography.scss';
+import './components/mixins/scrollbars.scss';
 import './components/header/header.scss';
 import './components/nav/nav.scss';
 import './components/footer/footer.scss';
+
+// CSS - IE Fixes
+import './components/ie/ie.scss';
 
 // CSS - Print styles
 import './components/base/print.scss';
